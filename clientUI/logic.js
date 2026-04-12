3ae6302741d686b8f9a2fcbffd78f6facd1eadb2
cdss8 <fabi.rayo2317@gmail.com>
2026-04-12 01:48:07 +0100

    12th commit:taiwildcss/vuejs/html

--- clientUI/logic.js
+++ clientUI/logic.js
@@ -1,39 +1,68 @@
-window.onload = function(){
-Vue.createApp({
-    data() {
-        return {
-            cuenta: {
-                field_balance: "999,999.00",
-                field_iban: "ES12 3456 7890 0000 0000",
-                field_account_type: "Cuenta de Prueba Local"
-            },
-
-            cuentadrupal: null,
-            error: null
-        }
-    },
-    mounted() {
-            const drupalUrl = 'http://fintechapp.ddev.site/jsonapi/account';
-
-            console.log("Conecting to Drupal at:", drupalUrl);
-            fetch(drupalUrl)
-                .then(response => {
-                    if (!response.ok) throw new Error("Cant connet with Drupal");
-                    return response.json();
-                })
-                .then(json => {
-                    //Drupal stores at data[0].attributes
+window.onload = function() {
+    Vue.createApp({
+        data() {
+            return {
+                cuenta: {
+                    field_balance: "999,999.00",
+                    field_iban: "ES12 3456 7890 0000 0000",
+                    field_account_type: "Cuenta de Prueba Local"
+                },
+                cuentadrupal: null,
+                error: null,
+                showTransfer: false,
+                transferAmount: null,
+                destination: '',
+                limits: null,
+                showLimits: false,
+                showLoans: false // Añadido para evitar errores
+            }
+        },
+        mounted() {
+            this.fetchLimits();
+            this.fetchAccount();
+        },
+        methods: {
+            async fetchAccount() {
+                const drupalUrl = 'http://fintechapp.ddev.site/jsonapi/account';
+                try {
+                    const response = await fetch(drupalUrl);
+                    if (!response.ok) throw new Error("Cant connect with Drupal");
+                    const json = await response.json();
                     if (json.data && json.data.length > 0) {
                         this.cuentadrupal = json.data[0].attributes;
-                        console.log("Drupal data received", this.cuentadrupal);
-                    } else {
-                        this.error = "Didnt found bank accounts";
                     }
-                })
-                .catch(err => {
-                    console.error("Error:", err);
-                    this.error = "CORS, or conection errors";
-                });
+                } catch (err) {
+                    this.error = "CORS, or connection errors";
+                }
+            },
+
+            async fetchLimits() {
+                const limitsUrl = 'http://fintechapp.ddev.site/jsonapi/limits_alerts';
+                try {
+                    const response = await fetch(limitsUrl);
+                    const json = await response.json();
+                    if (json.data && json.data.length > 0) {
+                        this.limits = json.data[0].attributes;
+                        console.log("Limits received", this.limits);
+                    }
+                } catch (err) {
+                    console.error("Error fetching limits:", err);
+                }
+            },
+
+            async makeTransfer() {
+                if (!this.transferAmount || this.transferAmount <= 0) return alert("Invalid amount");
+                
+                console.log(`Enviando ${this.transferAmount} a ${this.destination}`);
+                
+                if (this.cuentadrupal) {
+                    this.cuentadrupal.field_balance -= this.transferAmount;
+                    this.showTransfer = false;
+                    this.transferAmount = null;
+                    this.destination = '';
+                    alert("¡Transferencia realizada con éxito!");
+                }
+            }
         }
     }).mount('#app');
 };
